9c1f01a422cf9005ecfc7422048dbabd2fd1c089
Moralain <jormoluis@gmail.com>
2013-01-29 16:31:49 +0000

    Revert "Revert "Probando probando 1,2...1,2""
    This reverts commit 14ab62d34623d7d5ce37af4b2e5ca60d3cc54322.

---
+++ press.php
@@ -0,0 +1,14 @@
+<?php include "header.php" ?>
+	
+    <div id="container">
+	    	<div id="press">
+            	<ul>
+            		<li><a href="#">Link Press 1 </a></li>
+                	<li><a href="#">Link Press 2 </a></li>   		
+	    		</ul>
+            </div>
+	</div>
+    
+ 
+	 
+<?php include "footer.php" ?>
